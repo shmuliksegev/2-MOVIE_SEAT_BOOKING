1ada7d9ad3640055dfb637ee02a43522d45014a6
shmuliksegev <zzaaaazz6@gmail.com>
2021-01-19 14:17:36 +0200

    Save Data to Local Storage

--- script.js
+++ script.js
@@ -12,19 +12,29 @@ let ticketPrice = +movieSelected.value;
 // or convert by the function
 // const ticketPrice = parseInt(movieSelected.value)
 
+// Save selected movie and price
+function setMovieData(moiveIndex,moviePrice){
+  localStorage.setItem('selectedMovieIndex',moiveIndex);
+  localStorage.setItem('selectedMoviePrice',moviePrice);
+}
+
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsIndex = [...selectedSeats].map((item) =>
+    [...seat].indexOf(item)
+  );
+
+  localStorage.setItem('selectedSeatIndex',  JSON.stringify(selectedSeatsIndex));;
   const selectedSeatsCount = selectedSeats.length;
-   
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
-
-
 }
 // Movie click
 movieSelected.addEventListener('change',(e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 })
 
